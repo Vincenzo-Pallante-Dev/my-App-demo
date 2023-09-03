dca35bbe74ff1d4e0078b87cdaacb10d9924084b
Vincenzo-Pallante-Dev <vincenzopallante0@gmail.com>
2023-09-03 20:05:11 +0200

    feat: my-app

---
+++ my-app/src/components/Card.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default class Card extends React.Component {
+  render() {
+    return (
+      <div className="col">
+        <div className="card" style={{ width: "18rem", textAlign: "center" }}>
+          <button
+            onClick={() => this.props.onIncrement(this.props.card)}
+            className="btn btn-primary"
+          >
+            Aggiungi{" "}
+            <span className="badje badje-light">
+              {this.props.card.quantità}
+            </span>
+          </button>
+          <img
+            src={this.props.card.immagine}
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 clasNclassNames="card-title">{this.props.card.nome} Roll</h5>
+            <p className="card-text">€{this.props.card.prezzo}</p>
+            <button
+              onClick={() => this.props.onDelete(this.props.card.id)}
+              className="btn btn-outline-danger"
+            >
+              Elimina
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
